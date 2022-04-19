0f68d0a77890bf0519225ab7696c10e299e883fe
rizzpex <100021645+Rizz990@users.noreply.github.com>
2022-04-19 13:13:50 +0700

    Update __main__.py

--- SkyzuRobot/__main__.py
+++ SkyzuRobot/__main__.py
@@ -84,7 +84,7 @@ PM_START_TEXT = """
 *Hello {} !*
 ────────────────────────
 ✪ I'm an anime theme bot designed to help manage your telegram group with a lot features.
-✪ Maintained by @skyzu ✨
+✪ Maintained by @xfelzy ✨
 ────────────────────────
 Hit the /help to see available command.
 """
@@ -101,7 +101,7 @@ buttons = [
         InlineKeyboardButton(
             text="sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​", url="https://github.com/Skyzu/SkyzuRobot"
         ),
-        InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ​", url=f"https://t.me/{SUPPORT_CHAT}"),
+        InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ​", url=f"https://t.me/BlezzSupportt"),
     ],
     [
         InlineKeyboardButton(
@@ -117,7 +117,7 @@ Click on the button bellow to get description about specifics command."""
 EMI_IMG = "https://telegra.ph/file/56811b69cbcece20bbebf.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
- You can support the project by contacting @skyzu \
+ You can support the project by contacting @xfelzy \
  Supporting isnt always financial! \
  Those who cannot provide monetary support are welcome to help us develop the bot at ."""
 
